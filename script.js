33f761c1c9d72c9d9430b1e93174e53788f9fc90
kwagm <kwagm@amazon.com>
2021-11-13 13:33:27 -0500

    working on isgameover function first try not fully accurate.

--- script.js
+++ script.js
@@ -23,10 +23,35 @@ const displayController = (function () {
 })();
 
 
+
+
 function game() {
 
     const playerOne = playerFactory("Kenji", "X");
     const playerTwo = playerFactory("Schmo", "O");
+
+    function isGameOver() {
+        const board = myGameBoard.board;
+        if (board[0], board[4], board[8] ||
+            board[2], board[4], board[6] ||
+            board[0], board[1], board[2] ||
+            board[3], board[4], board[5] ||
+            board[6], board[7], board[8] ||
+            board[0], board[3], board[6] ||
+            board[1], board[4], board[7] ||
+            board[2], board[5], board[8] === 'X') {
+            console.log(` ${playerOne.name} Wins`)
+        } else if(board[0], board[4], board[8] ||
+            board[2], board[4], board[6] ||
+            board[0], board[1], board[2] ||
+            board[3], board[4], board[5] ||
+            board[6], board[7], board[8] ||
+            board[0], board[3], board[6] ||
+            board[1], board[4], board[7] ||
+            board[2], board[5], board[8] === 'O'){
+            console.log(` ${playerTwo.name} Wins`)
+        }
+    }
     player = playerOne;
     const tile = document.querySelectorAll(".tile");
     tile.forEach(element => {
@@ -34,13 +59,14 @@ function game() {
             if (player === playerOne && b[element.id] === '') {
                 b[element.id] = playerOne.mark;
                 player = playerTwo;
-            } else if(player === playerTwo && b[element.id] === ''){
+            } else if (player === playerTwo && b[element.id] === '') {
                 b[element.id] = playerTwo.mark;
                 player = playerOne;
-            }else{
+            } else {
                 return
             };
             displayController.setboard();
+            isGameOver();
         })
     });
 }
