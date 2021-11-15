20c8bdfc5cb1b5ff103d0e71e92dea8b8470b5cc
kwagm <kwagm@amazon.com>
2021-11-15 17:02:25 -0500

    interface working as intended

--- script.js
+++ script.js
@@ -29,8 +29,10 @@ function game() {
 
     const playerOne = playerFactory("Kenji", "X");
     const playerTwo = playerFactory("Schmo", "O");
+    const pTurn = document.querySelector('#turn');
     function isGameOver() {
         const board = myGameBoard.board;
+        const winDisplay = document.querySelector('#winner');
         if ((board[0] === 'X' && board[4] === 'X' && board[8] === 'X') ||
             (board[2] === 'X' && board[4] === 'X' && board[6] === 'X') ||
             (board[0] === 'X' && board[1] === 'X' && board[2] === 'X') ||
@@ -39,7 +41,10 @@ function game() {
             (board[0] === 'X' && board[3] === 'X' && board[6] === 'X') ||
             (board[1] === 'X' && board[4] === 'X' && board[7] === 'X') ||
             (board[2] === 'X' && board[5] === 'X' && board[8] === 'X')) {
-            console.log(` ${playerOne.name} Wins`)
+            console.log(` ${playerOne.name} Wins`);
+            winDisplay.style.border = "3px solid blue";
+            winDisplay.textContent = `The winner is ${playerOne.name}! CONGRATULATIONS!`;
+            pTurn.textContent = "GAME OVER!";
             player = '';
         } else if ((board[0] === 'O' && board[4] === 'O' && board[8] === 'O') ||
             (board[2] === 'O' && board[4] === 'O' && board[6] === 'O') ||
@@ -49,21 +54,27 @@ function game() {
             (board[0] === 'O' && board[3] === 'O' && board[6] === 'O') ||
             (board[1] === 'O' && board[4] === 'O' && board[7] === 'O') ||
             (board[2] === 'O' && board[5] === 'O' && board[8] === 'O')) {
-            console.log(` ${playerTwo.name} Wins`)
+            console.log(` ${playerTwo.name} Wins`);
+            winDisplay.style.border = "3px solid blue";
+            winDisplay.textContent = `The winner is ${playerTwo.name}! CONGRATULATIONS!`;
+            pTurn.textContent = "GAME OVER!";
             player = '';
         }
-        
+
     }
     player = playerOne;
+    pTurn.textContent = `It is ${player.name}'s turn!`;
     const tile = document.querySelectorAll(".tile");
     tile.forEach(element => {
         element.addEventListener('click', () => {
             if (player === playerOne && b[element.id] === '') {
                 b[element.id] = playerOne.mark;
                 player = playerTwo;
+               pTurn.textContent = `It is ${playerTwo.name}'s turn!`; 
             } else if (player === playerTwo && b[element.id] === '') {
                 b[element.id] = playerTwo.mark;
                 player = playerOne;
+                pTurn.textContent = `It is ${playerOne.name}'s turn!`;
             } else {
                 return
             };
