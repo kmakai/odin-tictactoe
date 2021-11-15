b9b927772dcdd8b9b9633f170438272c160b0260
kwagm <kwagm@amazon.com>
2021-11-15 15:59:21 -0500

    ...

--- script.js
+++ script.js
@@ -29,28 +29,30 @@ function game() {
 
     const playerOne = playerFactory("Kenji", "X");
     const playerTwo = playerFactory("Schmo", "O");
-
     function isGameOver() {
         const board = myGameBoard.board;
-        if (board[0], board[4], board[8] ||
-            board[2], board[4], board[6] ||
-            board[0], board[1], board[2] ||
-            board[3], board[4], board[5] ||
-            board[6], board[7], board[8] ||
-            board[0], board[3], board[6] ||
-            board[1], board[4], board[7] ||
-            board[2], board[5], board[8] === 'X') {
+        if ((board[0] === 'X' && board[4] === 'X' && board[8] === 'X') ||
+            (board[2] === 'X' && board[4] === 'X' && board[6] === 'X') ||
+            (board[0] === 'X' && board[1] === 'X' && board[2] === 'X') ||
+            (board[3] === 'X' && board[4] === 'X' && board[5] === 'X') ||
+            (board[6] === 'X' && board[7] === 'X' && board[8] === 'X') ||
+            (board[0] === 'X' && board[3] === 'X' && board[6] === 'X') ||
+            (board[1] === 'X' && board[4] === 'X' && board[7] === 'X') ||
+            (board[2] === 'X' && board[5] === 'X' && board[8] === 'X')) {
             console.log(` ${playerOne.name} Wins`)
-        } else if(board[0], board[4], board[8] ||
-            board[2], board[4], board[6] ||
-            board[0], board[1], board[2] ||
-            board[3], board[4], board[5] ||
-            board[6], board[7], board[8] ||
-            board[0], board[3], board[6] ||
-            board[1], board[4], board[7] ||
-            board[2], board[5], board[8] === 'O'){
+            player = '';
+        } else if ((board[0] === 'O' && board[4] === 'O' && board[8] === 'O') ||
+            (board[2] === 'O' && board[4] === 'O' && board[6] === 'O') ||
+            (board[0] === 'O' && board[1] === 'O' && board[2] === 'O') ||
+            (board[3] === 'O' && board[4] === 'O' && board[5] === 'O') ||
+            (board[6] === 'O' && board[7] === 'O' && board[8] === 'O') ||
+            (board[0] === 'O' && board[3] === 'O' && board[6] === 'O') ||
+            (board[1] === 'O' && board[4] === 'O' && board[7] === 'O') ||
+            (board[2] === 'O' && board[5] === 'O' && board[8] === 'O')) {
             console.log(` ${playerTwo.name} Wins`)
+            player = '';
         }
+        
     }
     player = playerOne;
     const tile = document.querySelectorAll(".tile");
